0cd9fb587379ea34bfeb31aa5d6693a570dff459
sadia <sadia@techhivebd.com>
2020-04-18 04:31:29 +0600

    crease basic redux structure for user

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import { cakeReducer } from './cake/cakeReducer';
 import icecreamReducer from './icecream/increamReducer';
-
+import userReducer from './user/userReducer'
 const rootReducer = combineReducers({
   cake: cakeReducer,
-  icecream: icecreamReducer
+  icecream: icecreamReducer,
+  user: userReducer
 })
 
 export default rootReducer;
