2d98cc20db0548114e85ff13284f12c4cfbf02c0
sadia <sadia@techhivebd.com>
2020-04-17 13:21:52 +0600

    connect action reducer store

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { buy_cake } from '../redux';
+import { connect } from 'react-redux';
 
-function CakeContainer() {
+function CakeContainer(props) {
+  console.log(props)
   return (
     <div>
-      <h2>No of cakes</h2>
-      <button>Buy Cake</button>
+      <h2>No of cakes = {props.noOfCake}</h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   )
 }
 
-export default CakeContainer;
+//step-1
+//selectors file
+const mapStateToProps = state => {
+  return {
+    noOfCake: state.noOfCake
+  }
+}
+
+//step-2 difining mapDispathToProps
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake: () => dispatch(buy_cake())
+  }
+}
+
+//step-3 connect 2 function with react component
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
--- src/redux/cake/cakeReducer.js
+++ src/redux/cake/cakeReducer.js
@@ -1,17 +1,17 @@
 import { buyCake } from './cakeTypes';
 
 const initialState = {
-  noOfCake = 10
+  noOfCake: 10
 }
 
 const cakeReducer = (state = initialState, action) => {
   switch (action.type) {
     case buyCake: return {
       ...state,
-      noOfCake = state.noOfCake - 1
+      noOfCake: state.noOfCake - 1
     }
     default: return state
   }
 }
 
-export default cakeReducer;
+export { cakeReducer };
