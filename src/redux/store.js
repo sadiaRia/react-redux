fe0459b6bf15e003890b287fd94f38b756109fee
sadia <sadia@techhivebd.com>
2020-04-18 01:55:37 +0600

    add logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer); {/*it takes a reducer */ }
+const store = createStore(rootReducer, applyMiddleware(logger)); {/*it takes a reducer */ }
 
 export default store;
