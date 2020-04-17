2a97cac162057593fcf8d83afbe3a931f01985ee
sadia <sadia@techhivebd.com>
2020-04-17 22:48:15 +0600

    update side note

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -17,6 +17,7 @@ function CakeContainer(props) {
 // this function return the value which component requires
 // it can take 2 parameter (state,ownProps)
 //It is called every time the store state changes.
+//it receieves redux state as papmeter & retrieve state properties
 const mapStateToProps = state => {
   return {
     noOfCake: state.noOfCake
@@ -24,6 +25,8 @@ const mapStateToProps = state => {
 }
 
 //step-2 difining mapDispathToProps
+//dispathing action => get dismatch method as parmeter & allows us action creator to props
+//this allows us to call props.buyCake
 const mapDispatchToProps = dispatch => {
   return {
     buyCake: () => dispatch(buy_cake())
